29446f548980f9d7ad940a6f888519df87f904ba
Mikael Reponen <mikael.reponen@gmail.com>
2023-12-07 22:01:50 +0200

    The wasm is now compiled into a single file for a more convenient inclusion in other apps

--- wasm/make.sh
+++ wasm/make.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 make scsynth
 cp server/scsynth/*.js ../wasm/
-cp server/scsynth/*.wasm ../wasm/
+cp server/scsynth/*.mjs ../wasm/
+#cp server/scsynth/*.wasm ../wasm/
 
 cp server/scsynth/*.js ../wasmSuperCollider/
-cp server/scsynth/*.wasm ../wasmSuperCollider/
+cp server/scsynth/*.mjs ../wasmSuperCollider/
+#cp server/scsynth/*.wasm ../wasmSuperCollider/
 
 cp ../wasm/sc_wasm.js ../wasmSuperCollider/
